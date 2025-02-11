a0aeb38676cfc66040f65ede5716480c572880b0
timok81 <timokujansuu@gmail.com>
2025-02-11 09:06:31 +0100

    Revised to use beforeEach()

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -4,21 +4,21 @@ import userEvent from "@testing-library/user-event";
 import NumberOfEvents from "../components/NumberOfEvents";
 
 describe("<NumberOfEvents /> component", () => {
-  test("the component contains an input field", () => {
+  let numberInput;
+  beforeEach(() => {
     const numberComponent = render(<NumberOfEvents />);
-    const numberInput = numberComponent.queryByRole("textbox");
+    numberInput = numberComponent.queryByRole("textbox");
+  });
+
+  test("the component contains an input field", () => {
     expect(numberInput).toBeInTheDocument();
   });
 
   test("the initial input field value is 32", () => {
-    const numberComponent = render(<NumberOfEvents />);
-    const numberInput = numberComponent.queryByRole("textbox");
     expect(numberInput).toHaveValue("32");
   });
 
   test("the value in input field changes when the user types in a new number", async () => {
-    const numberComponent = render(<NumberOfEvents />);
-    const numberInput = numberComponent.queryByRole("textbox");
     const user = userEvent.setup();
     await user.type(numberInput, "{backspace}{backspace}10");
     expect(numberInput).toHaveValue("10");
