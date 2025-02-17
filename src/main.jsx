552043101cce606a2e5398babf19144c9d1fdb03
timok81 <timokujansuu@gmail.com>
2025-02-17 09:23:48 +0100

    Added app manifest

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import * as atatus from 'atatus-spa';
-atatus.config('0d8df3ece8c04f7baa26f6a4ac01e484').install();
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import * as atatus from "atatus-spa";
+atatus.config("0d8df3ece8c04f7baa26f6a4ac01e484").install();
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
+serviceWorkerRegistration.register();
