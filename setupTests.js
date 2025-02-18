fb7c46bf171d4475c7bb75c00444c0aa9c0ee1ac
timok81 <timokujansuu@gmail.com>
2025-02-18 10:38:46 +0100

    Added code to avoid errors during testing due to charts

--- setupTests.js
+++ setupTests.js
@@ -17,3 +17,21 @@ console.error = (...args) => {
 };
 
 jest.setTimeout(30000);
+
+//Solves "ReferenceError: ResizeObserver is not defined" error during testing
+const { ResizeObserver } = window;
+
+beforeEach(() => {
+  //@ts-ignore
+  delete window.ResizeObserver;
+  window.ResizeObserver = jest.fn().mockImplementation(() => ({
+    observe: jest.fn(),
+    unobserve: jest.fn(),
+    disconnect: jest.fn(),
+  }));
+});
+
+afterEach(() => {
+  window.ResizeObserver = ResizeObserver;
+  jest.restoreAllMocks();
+});
